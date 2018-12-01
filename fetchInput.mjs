b9b5331bf5fb7fd6f64238a990d836f418cef68e
Josip Igrec <me@allypost.net>
2018-12-01 13:09:08 +0100

    Input is now trimmed of whitespace

--- fetchInput.mjs
+++ fetchInput.mjs
@@ -9,6 +9,7 @@ export const fetch = (day, withLog = true) => {
     return (
         Fetch(`https://adventofcode.com/2018/day/${ day }/input`, { headers: { cookie: fs.readFileSync('cookie.txt', 'utf-8').trim() } })
             .then(res => res.text())
+            .then(res => res.trim())
             .then((res) => {
                 if (withLog)
                     Log.result(`Fetched`);
