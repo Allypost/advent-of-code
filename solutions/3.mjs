8d482520e2ffa8dcddd300a071335f4be8a485ad
Josip Igrec <me@allypost.net>
2018-12-03 08:16:05 +0100

    Added day 3 solution

---
+++ solutions/3.mjs
@@ -0,0 +1,95 @@
+import { Solution } from './solution';
+
+class Patch {
+    constructor(str) {
+        const [ id, , offset, size ] = str.split(' ');
+
+        const [ left, top ] =
+            offset
+                .substr(0, offset.length - 1)
+                .split(',')
+                .map(x => Number(x) - 1);
+
+        const [ width, height ] =
+            size
+                .split('x')
+                .map(Number);
+
+        this.id = Number(id.substr(1));
+        this.dimensions = { width, height };
+        this.offset = { top, left };
+        this.overlaps = false;
+    }
+
+    draw(arr, obj = null) {
+        const { left, top } = this.offset;
+        const { width, height } = this.dimensions;
+
+        if (obj)
+            obj[ this.id ] = this;
+
+        for (let y = top; y < top + height; y++) {
+            while (arr.length <= y)
+                arr.push([]);
+
+            for (let x = left; x < left + width; x++) {
+                while (arr[ y ].length < x)
+                    arr[ y ].push(null);
+
+                const val = arr[ y ][ x ];
+
+                if (!val) {
+                    arr[ y ][ x ] = this.id;
+                    continue;
+                }
+
+                arr[ y ][ x ] = 'X';
+
+                if (!obj)
+                    continue;
+
+                this.overlaps = true;
+
+                if (val !== 'X')
+                    obj[ val ].overlaps = true;
+            }
+        }
+    }
+}
+
+export class Day3 {
+    DAY = 3;
+    #data = '';
+    #grid = [];
+    #obj = {};
+
+    constructor() {
+        new Solution().bindTo(this);
+    }
+
+    part1(data = this.#data) {
+        this.#grid = [];
+        return (
+            Promise
+                .resolve(data)
+                .then(res => res.trim().split('\n'))
+                .then(data => data.map(line => new Patch(line)))
+                .then(data => data.forEach(patch => patch.draw(this.#grid)))
+                .then(() => this.#grid.reduce((acc, line) => acc + line.filter(patch => patch === 'X').length, 0))
+                .then(data => this._trigger('success', 1, data))
+        );
+    }
+
+    part2(data = this.#data) {
+        this.#grid = [];
+        return (
+            Promise
+                .resolve(data)
+                .then(res => res.trim().split('\n'))
+                .then(data => data.map(line => new Patch(line)))
+                .then(data => data.forEach(e => e.draw(this.#grid, this.#obj)))
+                .then(() => Object.values(this.#obj).find(o => !o.overlaps).id)
+                .then(data => this._trigger('success', 2, data))
+        );
+    }
+}
--- solutions/solutions.mjs
+++ solutions/solutions.mjs
@@ -1,4 +1,5 @@
 import { Day1 } from './1';
 import { Day2 } from './2';
+import { Day3 } from './3';
 
-export default [ Day1, Day2 ];
+export default [ Day1, Day2, Day3 ];
