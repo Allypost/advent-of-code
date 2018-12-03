78807e34c472ab75ccb6cb6aa02a6015c71edbd9
Josip Igrec <me@allypost.net>
2018-12-03 08:41:17 +0100

    Now assuming that the cookie.txt contains only the session cookie value

--- fetchInput.mjs
+++ fetchInput.mjs
@@ -7,7 +7,7 @@ export const fetch = (day, withLog = true) => {
         Log.progress(`Fetching...`);
 
     return (
-        Fetch(`https://adventofcode.com/2018/day/${ day }/input`, { headers: { cookie: fs.readFileSync('cookie.txt', 'utf-8').trim() } })
+        Fetch(`https://adventofcode.com/2018/day/${ day }/input`, { headers: { cookie: `session=${ fs.readFileSync('cookie.txt', 'utf-8').trim() }` } })
             .then(res => res.text())
             .then(res => res.trim())
             .then((res) => {
