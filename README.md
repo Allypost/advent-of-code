184ea6a94454c7adec51fcbfdd0d89072dbc01aa
Josip Igrec <me@allypost.net>
2018-12-03 10:01:21 +0100

    Added way to select which days to run via CLI

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Most scripts are cobbled together monstrosities but, in my opinion, that's part 
 2. Run the good ol' `npm install`
 3. Edit the cookie.txt with your `session` cookie value from [Advent of Code](https://adventofcode.com)
 4. Run `npm start` and it should fetch the inputs and solve the challenges day by day
+5. (OPTIONAL) You can run `npm start` with a list (either space or comma separated) of days to run (eg. `npm start 5 13` or `npm start 5,13` to run just challenges for days 5 and 13)
--- solutions/solutions.mjs
+++ solutions/solutions.mjs
@@ -2,4 +2,39 @@ import { Day1 } from './1';
 import { Day2 } from './2';
 import { Day3 } from './3';
 
-export default [ Day1, Day2, Day3 ];
+Array.prototype.flat = Array.prototype.flat || function(depth) {
+    return (
+        this.reduce(
+            (acc, el) => (
+                [
+                    ...acc,
+                    ...(
+                        Array.isArray(el) && depth > 0
+                        ? el.flat(depth - 1)
+                        : [ el ]
+                    ),
+
+                ]
+            ),
+            [],
+        )
+    );
+};
+
+Array.prototype.flatMap = Array.prototype.flatMap || function(...args) {
+    return this.map(...args).flat(1);
+};
+
+const args =
+    process.argv
+           .slice(2)
+           .flatMap(arg => arg.split(','))
+           .map(Number)
+           .filter(arg => Number.isInteger(arg));
+
+const days =
+    [ Day1, Day2, Day3 ]
+        .filter(day => args.includes(day.DAY))
+        .sort((a, b) => a.DAY - b.DAY);
+
+export default days;
